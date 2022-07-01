81e8063ce59687b93762bfcab7924a3d9a8860af
Shallon Kobusinge <kobshallon041@gmail.com>
2022-07-02 01:17:32 +0200

    feat: starter

---
+++ utils/headers.h
@@ -0,0 +1,26 @@
+//
+// Created by kobsh on 02/07/2022.
+//
+
+//
+// Created by kobsh on 01/07/2022.
+//
+#include "iostream"
+#include "string"
+#include "vector"
+#include "fstream"
+#include "stdio.h"
+#include "sstream"
+#include "stdlib.h"
+#include "ctime"
+#include <cstring>
+#include <cstdlib>
+#include <stdexcept>
+#include <iterator>
+#include <ctime>
+#include <iomanip>
+#include <codecvt>
+
+#pragma once
+
+using namespace std;
